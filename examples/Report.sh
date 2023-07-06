2ce39de5237a330c919c17eddf24b1d0306bca71
KraSerh <kraserh@gmail.com>
2023-07-06 18:18:00 +0300

    Створення каталогу Output не виводе помилку.

--- examples/Report.sh
+++ examples/Report.sh
@@ -3,7 +3,7 @@
 WORKDIR=Output
 JOBNAME=report
 
-mkdir $WORKDIR
+mkdir -p $WORKDIR
 cat $JOBNAME.tex.tmpl | energozvit-tmpl "$1" $2 > $WORKDIR/$JOBNAME.tex
 cd $WORKDIR
 pdflatex -interaction=nonstopmode $JOBNAME.tex \
